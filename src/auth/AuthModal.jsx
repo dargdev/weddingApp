8e220d843be4a437e52d9ba6082a4949242838f8
David Ruano <dargruano@gmail.com>
2024-06-06 19:09:51 -0500

    Fix login

--- src/auth/AuthModal.jsx
+++ src/auth/AuthModal.jsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 import { useAuth } from './AuthContext';
 
 Modal.setAppElement('#root');
 
 const AuthModal = () => {
-  const { authenticate } = useAuth();
+  const { authenticate, isAuthenticated } = useAuth();
   const [phone, setPhone] = useState('');
   const [modalIsOpen, setModalIsOpen] = useState(true);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     authenticate(phone);
-    setModalIsOpen(false);
   };
+  useEffect(() => {
+    if (isAuthenticated) {
+      setModalIsOpen(false);
+    } else {
+      setModalIsOpen(true);
+    }
+  }, [isAuthenticated]);
 
   return (
     <Modal
@@ -27,7 +33,7 @@ const AuthModal = () => {
         <input
           type="tel"
           value={phone}
-          onChange={(e) => setPhone(e.target.value)}
+          onChange={e => setPhone(e.target.value)}
           placeholder="Ingresa tu numero celular"
           required
           pattern="\d*"
