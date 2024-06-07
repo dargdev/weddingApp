e13554bfdd7213a1ce071657ba7a9e8e1dbc3723
David Ruano <dargruano@gmail.com>
2024-06-06 19:03:35 -0500

    Fix background menu

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,24 +3,26 @@ import anillo1 from '../photos/anillo-1.jpeg';
 import anillo2 from '../photos/anillo-2.jpeg';
 function HeroSection() {
   return (
-    <div className="hero-section backgorund-image-ken-burns">
+    <div id="home" className="hero-section backgorund-image-ken-burns">
       <div className="slideshow">
-        <div className="slideshow-image" style={{backgroundImage: `url(${anillo1})`}}></div>
-        <div className="slideshow-image anillo2" style={{backgroundImage: `url(${anillo2})`}}></div>
+        <div
+          className="slideshow-image"
+          style={{ backgroundImage: `url(${anillo1})` }}
+        ></div>
+        <div
+          className="slideshow-image anillo2"
+          style={{ backgroundImage: `url(${anillo2})` }}
+        ></div>
       </div>
       <div className="container hero-inner">
         <div className="row">
           <div className="col-md-12 text-center">
             <h1 className="hero-title">Ariana Y David</h1>
             <p className="hero-subtitle">17 Agosto, 2024 – Quito</p>
-            <div className="arrow bounce text-center">
-              <a href="#couple"> <i className="ti-heart"></i> </a>
-            </div>
           </div>
         </div>
       </div>
     </div>
-    
   );
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,66 +9,73 @@ function Sidebar() {
   return (
     <>
       <button
-        className={`js-oliven-nav-toggle oliven-nav-toggle ${isOpen ? 'active' : ''} menu-icon-button`}
+        className={`oliven-nav-toggle ${isOpen ? 'active' : ''}`}
         onClick={handleMenuToggle}
       >
-        <div className="menu-icon"></div>
+        <div
+          className={`oliven-nav-toggle-icon ${isOpen ? 'active' : ''}`}
+        ></div>
       </button>
-      <div className={isOpen ? 'oliven-aside open' : 'oliven-aside'}>
-        <div className="oliven-logo">
-          <a href="index.html">
-            <img src="images/logo.png" alt="" />
-            <span>
-              Ariana <small>y</small> David
-            </span>
-            <h6>17.08.2024</h6>
-          </a>
-        </div>
-        <nav className="oliven-main-menu">
-          <ul>
-            <li>
-              <a href="#home" onClick={handleMenuToggle}>
-                Inicio
-              </a>
-            </li>
-            <li>
-              <a href="#couple" onClick={handleMenuToggle}>
-                Pareja
-              </a>
-            </li>
-            <li>
-              <a href="#story" onClick={handleMenuToggle}>
-                Nuestra Historia
-              </a>
-            </li>
-            <li>
-              <a href="#whenwhere" onClick={handleMenuToggle}>
-                ¿Cuándo y Dónde?
-              </a>
-            </li>
-            <li>
-              <a href="#rsvp" onClick={handleMenuToggle}>
-                Confirma tu asistencia
-              </a>
-            </li>
-            <li>
-              <a href="#gift" onClick={handleMenuToggle}>
-                Agradecemos tu gentileza
-              </a>
-            </li>
-            <li>
-              <a href="#gallery" onClick={handleMenuToggle}>
-                Galería
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="footer1">
-          <span className="separator"></span>
-          <p>
-            Boda de Ariana & David <br />
-            17 Agosto 2024, Quito
-          </p>
+      <div
+        className={`oliven-aside-container ${isOpen ? 'active' : ''}`}
+        onClick={handleMenuToggle}
+      >
+        <div className={`oliven-aside ${isOpen ? 'open' : ''}`}>
+          <div className="oliven-logo">
+            <a href="index.html">
+              <img src="images/logo.png" alt="" />
+              <span>
+                Ariana <small>y</small> David
+              </span>
+              <h6>17.08.2024</h6>
+            </a>
+          </div>
+          <nav className="oliven-main-menu">
+            <ul>
+              <li>
+                <a href="#home" onClick={handleMenuToggle}>
+                  Inicio
+                </a>
+              </li>
+              <li>
+                <a href="#couple" onClick={handleMenuToggle}>
+                  Pareja
+                </a>
+              </li>
+              <li>
+                <a href="#story" onClick={handleMenuToggle}>
+                  Nuestra Historia
+                </a>
+              </li>
+              <li>
+                <a href="#whenwhere" onClick={handleMenuToggle}>
+                  ¿Cuándo y Dónde?
+                </a>
+              </li>
+              <li>
+                <a href="#rsvp" onClick={handleMenuToggle}>
+                  Confirma tu asistencia
+                </a>
+              </li>
+              <li>
+                <a href="#gift" onClick={handleMenuToggle}>
+                  Agradecemos tu gentileza
+                </a>
+              </li>
+              <li>
+                <a href="#gallery" onClick={handleMenuToggle}>
+                  Galería
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="footer1">
+            <span className="separator"></span>
+            <p>
+              Boda de Ariana & David <br />
+              17 Agosto 2024, Quito
+            </p>
+          </div>
         </div>
       </div>
     </>
